9d657d267db67e9aba8e30d8ee81b674ab9f7ec7
Dominik <bumboxica@gmail.com>
2023-02-06 21:53:10 +0100

    Module useless comment deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,36 +15,13 @@ import { MatListModule } from '@angular/material/list';
 import { TableComponent } from './table/table.component';
 import {CdkDropList} from "@angular/cdk/drag-drop";
 import {DragDropModule} from '@angular/cdk/drag-drop';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import {MatMenuModule} from "@angular/material/menu";
-=======
-<<<<<<< Updated upstream
-=======
-import {MatMenuModule} from "@angular/material/menu";
->>>>>>> Stashed changes
->>>>>>> b759d90 (Base fix)
-=======
->>>>>>> 74919dd (app module configration fix)
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SidebarComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
     TableComponent,
-=======
-<<<<<<< Updated upstream
-    TableComponent
-=======
-    TableComponent,
->>>>>>> Stashed changes
->>>>>>> b759d90 (Base fix)
-=======
-    TableComponent,
->>>>>>> 74919dd (app module configration fix)
   ],
     imports: [
         BrowserModule,
@@ -57,19 +34,7 @@ import {MatMenuModule} from "@angular/material/menu";
         MatButtonModule,
         MatListModule,
         CdkDropList,
-<<<<<<< HEAD
-<<<<<<< HEAD
         DragDropModule,
-=======
-<<<<<<< Updated upstream
-        DragDropModule
-=======
-        DragDropModule,
->>>>>>> Stashed changes
->>>>>>> b759d90 (Base fix)
-=======
-        DragDropModule,
->>>>>>> 74919dd (app module configration fix)
     ],
   providers: [],
   bootstrap: [AppComponent]
