2bb22894db0de0b60b1fb076b540ec2592385a45
Dominik <bumboxica@gmail.com>
2023-03-13 01:55:16 +0100

    Implementation of not working secuity (elements with task backend not showing)

--- src/app/auth-services/auth.service.ts
+++ src/app/auth-services/auth.service.ts
@@ -10,14 +10,14 @@ export class AuthService {
 
   login(username:string,password:string){
     const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
-    return this.http.get("http://localhost:8081/",{headers,responseType: 'text' as 'json'})
+    return this.http.get("http://localhost:8081/api/",{headers,responseType: 'text' as 'json'})
   }
 
   getUsers() {
     let username='javatechie'
     let password='jt143'
     const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
-    return  this.http.get("http://localhost:8081/getUsers",{headers});
+    return  this.http.get("http://localhost:8081/api/getUsers",{headers});
   }
 }
 
