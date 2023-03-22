b6aa8a325b8b0cef9c63c3bd9b7dc0ae628cac0a
Dominik <bumboxica@gmail.com>
2023-03-23 00:14:08 +0100

    login fix part1

--- src/app/auth-services/auth.service.ts
+++ src/app/auth-services/auth.service.ts
@@ -5,19 +5,19 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
   providedIn: 'root'
 })
 export class AuthService {
- // private  baseUrl = "http://localhost:8080/";
+  private  baseUrl = "http://localhost:8081/";
   constructor(private http: HttpClient) { }
 
   login(username:string,password:string){
     const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
-    return this.http.get("http://localhost:8081/api/",{headers,responseType: 'text' as 'json'})
+    return this.http.get(`${(this.baseUrl)}`,{headers,responseType: 'text' as 'json'})
   }
 
   getUsers() {
-    let username='javatechie'
-    let password='jt143'
+    let username='dominik'
+    let password='123'
     const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
-    return  this.http.get("http://localhost:8081/api/getUsers",{headers});
+    return  this.http.get(`${(this.baseUrl)}getUsers`,{headers});
   }
 }
 
