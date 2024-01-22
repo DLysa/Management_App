644bd62cd7b3d7c64224c6135b613235778b337d
DLysa <dominik.lysakk@gmail.com>
2024-01-22 03:37:16 +0100

    Comment refresh

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,41 +1,36 @@
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
-import { Comment} from "../comment";
-import {CommentService} from "../services/sevices/comment.service";
-import {Task} from "../task";
-import {Store} from "../store/store";
-import {MatDialogRef} from "@angular/material/dialog";
-import {TaskDetailsFormComponent} from "../task-details-form/task-details-form.component";
-
+import {Component, OnInit} from '@angular/core';
+import { Comment } from "../comment";
+import { CommentService } from "../services/sevices/comment.service";
+import { Task } from "../task";
+import { Store } from "../store/store";
+import { MatDialogRef } from "@angular/material/dialog";
+import { TaskDetailsFormComponent } from "../task-details-form/task-details-form.component";
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.css']
 })
-export class CommentsComponent implements OnInit{
-
+export class CommentsComponent implements OnInit {
   private selectedTask: Task;
   comments: Comment[];
-  comments2: Comment[];
   isInputVisible: boolean = false;
-  commentText: any;
+  commentText: string;
 
   constructor(private commentService: CommentService,
-              private store: Store,
-              private dialogRef: MatDialogRef<TaskDetailsFormComponent>,) {
+              private store: Store) {
     this.selectedTask = store.selectedTask;
   }
 
-  ngOnInit():void {
-    this.commentService.getAllComments().subscribe((data:Comment[]) => {
-      console.log(data);
-      this.comments2=data.filter(
-        data=>data.taskId===this.selectedTask.id);
-      this.comments2=[...this.comments2].reverse();
+  ngOnInit(): void {
+    this.loadComments();
+  }
 
-      this.comments=data;
-      console.log(this.comments2)
-    })
+  loadComments(): void {
+    this.commentService.getAllComments().subscribe((data: Comment[]) => {
+      this.comments = data.filter(comment => comment.taskId === this.selectedTask.id);
+      this.comments.reverse();
+    });
   }
 
   toggleInput() {
@@ -43,28 +38,25 @@ export class CommentsComponent implements OnInit{
   }
 
   saveComment() {
-    const data ={
-      text : this.commentText,
-      status : this.selectedTask.status,
+    const newComment = {
+      text: this.commentText,
+      status: this.selectedTask.status,
       taskId: this.selectedTask.id,
       authorId: `${this.store.currentUser.firstName} ${this.store.currentUser.lastName}`
-    }
+    };
 
-    this.commentService.addComment(data)
-      .subscribe({
-        next: (res) => {
-          console.log(res);
-        },
-        error: (e) => console.error(e)
-      });
+    this.commentService.addComment(newComment).subscribe({
+      next: (res) => {
+        this.comments.unshift(res);
+        this.toggleInput();
+        this.clearInput();
+      },
+      error: (e) => console.error(e)
+    });
   }
 
   clearInput() {
-    this.commentText=""
+    this.commentText = "";
   }
 
-  close() {
-    this.dialogRef.close();
-
-  }
 }
